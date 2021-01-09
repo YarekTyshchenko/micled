eb19fd8040f7308a4456771da6aa2562ea22316f
Yarek Tyshchenko <yarekt@gmail.com>
2021-01-09 14:30:22 +0000

    Make it toggle properly

--- micled.sh
+++ micled.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-led=0
-while true; do
-    if amixer -D pulse get Capture | grep -qF '[on]'; then
-        if [[ "x$led" == "x0" ]]; then
-            led=1
-            xset led 3
-        fi
+function checkLed {
+    if amixer -D pulse get Capture | grep -F '[on]'; then
+        notify-send Mic On
+        xset led 3
     else
-        if [[ "x$led" == "x1" ]]; then
-            led=0
-            xset -led 3
-        fi
+        notify-send Mic Off
+        xset -led 3
     fi
-    sleep 1;
-done
+}
+
+checkLed;
+pactl subscribe | grep --line-buffered "Event 'change' on source" | (
+    while read -r LINE; do
+        checkLed;
+    done
+)
 
--- toggle.sh
+++ toggle.sh
@@ -1,4 +1,55 @@
 #!/bin/bash
+# Turn mic on/off or toggle it based on press delay
+# Start -> On
+# Press -> On
+# Quick release -> Off
+# Slow release -> Off
 
-amixer -D pulse set Capture toggle
+# Start with Off
+# Press toggles it to On
+# Quick release leaves it as On
+# Slow release turns it back Off
 
+timefile=/tmp/micled_timefile
+delay=250
+amixer_option="name='Capture Switch' -D pulse"
+
+function toggleCapture {
+    eval amixer cset $amixer_option toggle
+}
+function onCapture {
+    eval amixer cset $amixer_option on
+}
+function offCapture {
+    eval amixer cset $amixer_option off
+}
+function isOn {
+    eval amixer cget $amixer_option | grep -qF 'values=on'
+}
+
+case $1 in
+    press)
+        # notify-send Mic Press
+        # Check if its already on
+        if ! isOn; then
+            onCapture;
+            date +%s%3N > $timefile
+        fi
+        ;;
+    release)
+        # If we release after cutoff, toggle again.
+        if [[ -f $timefile ]]; then
+            pressStart=$(cat $timefile)
+            pressEnd=$(date +%s%3N)
+            (( difference=pressEnd-pressStart ))
+            echo Start $pressStart, End $pressEnd, diff: $difference
+            # notify-send Mic "Release: Diff $difference"
+            if [[ $difference -gt $delay ]]; then
+                toggleCapture
+            fi
+        else
+            offCapture
+        fi
+        rm $timefile ||:
+        ;;
+esac
